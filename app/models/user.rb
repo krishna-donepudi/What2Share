755e7ea88be921871ce469dee93e7a2eeda7cf55
Lucy <meixuanl@andrew.cmu.edu>
2020-11-13 14:50:31 -0500

    all ready except one last bug on adding apps with uppercase

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
   # relations
   has_one :partner, :class_name => 'User', :foreign_key => 'partner_id'
   belongs_to :partner, :class_name => 'User', optional: true
-  has_many :user_apps
+  has_many :user_apps, dependent: :destroy
   has_many :apps, through: :user_apps
 
   accepts_nested_attributes_for :apps
