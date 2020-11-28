4b6cba9e630b6aa496d763f8b9765d756e1ee10b
Lucy <meixuanl@andrew.cmu.edu>
2020-11-27 20:20:20 -0500

    update willingness to share options

--- app/models/user_app.rb
+++ app/models/user_app.rb
@@ -2,5 +2,5 @@ class UserApp < ApplicationRecord
   belongs_to :user
   belongs_to :app
 
-  WILL_TO_SHARE_OPTIONS = [['Unwilling to share', 'unwilling'],['Likely to share', 'likely'],['Willing to share', 'willing']].freeze
+  WILL_TO_SHARE_OPTIONS = [['Unwilling to share', 'unwilling'],['Neutral', 'neutral'],['Willing to share', 'willing'],['Not sure', 'notsure']].freeze
 end
